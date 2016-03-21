75e645ce69bc2a92b55283750a4180070e39fc61
haidang30 <ngohaidang30@gmail.com>
2016-03-21 14:53:39 +0700

    creating the Basic UI - add browse layout - created FlickrImageViewHolder class

---
+++ DemoFlickrBrowser/app/src/main/java/com/icestone/demoflickrbrowser/FlickrRecyclerViewAdapter.java
@@ -0,0 +1,10 @@
+package com.icestone.demoflickrbrowser;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by IceStone on 3/21/2016.
+ */
+public class FlickrRecyclerViewAdapter extends RecyclerView.Adapter<FlickrImageViewHolder>{
+    
+}
