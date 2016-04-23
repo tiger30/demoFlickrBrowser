a03c1296e852ea6689100d32dc4f736e4c5c612d
haidang30 <ngohaidang30@gmail.com>
2016-04-23 07:22:44 +0700

    81: completed

--- DemoFlickrBrowser/app/src/main/java/com/icestone/demoflickrbrowser/ViewPhotoDetailActivity.java
+++ DemoFlickrBrowser/app/src/main/java/com/icestone/demoflickrbrowser/ViewPhotoDetailActivity.java
@@ -2,6 +2,10 @@ package com.icestone.demoflickrbrowser;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class ViewPhotoDetailActivity extends BaseActivity {
 
@@ -13,6 +17,20 @@ public class ViewPhotoDetailActivity extends BaseActivity {
 
         Intent intent = getIntent();
         Photo photo = (Photo) intent.getSerializableExtra(PHOTO_TRANSFER);
+        TextView photoTitle = (TextView) findViewById(R.id.photo_title);
+        photoTitle.setText("Title: " + photo.getTitle());
+
+        TextView photoTags = (TextView) findViewById(R.id.photo_tags);
+        photoTags.setText("Tags: " + photo.getTags());
+
+        TextView photoAuthor = (TextView) findViewById(R.id.photo_author);
+        photoAuthor.setText(photo.getAuthor());
+
+        ImageView photoImage = (ImageView) findViewById(R.id.photo_image);
+        Picasso.with(this).load(photo.getLink())
+                .error(R.drawable.im_placeholder)
+                .placeholder(R.drawable.im_placeholder)
+                .into(photoImage);
 
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
